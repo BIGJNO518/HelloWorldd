2b1bf29bb99ac440d6e8859294c6aab5ab2ddd6c
Jason P Nowak <BIGJNO518@users.noreply.github.com>
2016-09-27 10:54:31 -0400

    Create Hello.java
    This is just practice. some fun learning github

---
+++ Hello.java
@@ -0,0 +1,14 @@
+
+public class Hello
+{
+ public static void main (String[]args) 
+ {
+  String firstName = "Jason";
+  String lastName = "Nowak";
+  
+ 
+ System.out.print ln (firstName + lastName);
+ 
+ 
+ }
+}
